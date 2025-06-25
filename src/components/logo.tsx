7c5c180420215ccfec7e029747077c1c9ab76a14
Carlos Ernesto Gómez Sánchez <gscarlosernesto@gmail.com>
2025-06-25 20:56:12 +0000

    faltó una miguita hacia la izquierda y hacia abahjo. solo una miguit.

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -60,8 +60,8 @@ export function PlesGroupLogo({ className, style }: { className?: string, style?
           width: 0.31em;
           height: 0.31em;
           
-          top: 0.38em;
-          left: 0.22em;
+          top: 0.40em;
+          left: 0.20em;
           transition: background-color 0.2s ease-out;
           transition-delay: 0s; /* Dot changes with the 'p' */
         }
