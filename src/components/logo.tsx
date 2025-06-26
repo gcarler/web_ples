389230591247c0ad42acc814c0e28b817b7a91fc
Carlos Ernesto Gómez Sánchez <gscarlosernesto@gmail.com>
2025-06-26 05:04:43 +0000

    TIENE QUE SER UN POCO MÁS PEQUEÑO QEU LA LETRA P

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -75,11 +75,11 @@ export function PlesGroupLogo({ className, style, hoverVariant }: { className?: 
           border-radius: 100%;
           transition: background-color 0.3s ease;
           
-          width: 0.40em;
-          height: 0.40em;
+          width: 0.38em;
+          height: 0.38em;
           
-          top: 0.33em;
-          left: 0.17em;
+          top: 0.34em;
+          left: 0.18em;
           z-index: -1;
 
           transform-origin: center;
@@ -90,10 +90,10 @@ export function PlesGroupLogo({ className, style, hoverVariant }: { className?: 
         .letra-p-con-punto::after {
           content: '';
           position: absolute;
-          width: 0.40em;
-          height: 0.40em;
-          top: 0.33em;
-          left: 0.17em;
+          width: 0.38em;
+          height: 0.38em;
+          top: 0.34em;
+          left: 0.18em;
           z-index: -2; /* Behind the main dot */
           background-color: hsl(var(--primary));
           border-radius: 100%;
