a4ba8c34beefc462372c92ca13e5c98e92e9b002
Carlos Ernesto Gómez Sánchez <gscarlosernesto@gmail.com>
2025-06-26 05:06:18 +0000

    DEBE SER MÁS COMO LA IMAGEN, GAURDAR LAS MISMAS PROPORCIONES

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -75,11 +75,11 @@ export function PlesGroupLogo({ className, style, hoverVariant }: { className?: 
           border-radius: 100%;
           transition: background-color 0.3s ease;
           
-          width: 0.38em;
-          height: 0.38em;
+          width: 0.35em;
+          height: 0.35em;
           
-          top: 0.34em;
-          left: 0.18em;
+          top: 0.355em;
+          left: 0.195em;
           z-index: -1;
 
           transform-origin: center;
@@ -90,10 +90,10 @@ export function PlesGroupLogo({ className, style, hoverVariant }: { className?: 
         .letra-p-con-punto::after {
           content: '';
           position: absolute;
-          width: 0.38em;
-          height: 0.38em;
-          top: 0.34em;
-          left: 0.18em;
+          width: 0.35em;
+          height: 0.35em;
+          top: 0.355em;
+          left: 0.195em;
           z-index: -2; /* Behind the main dot */
           background-color: hsl(var(--primary));
           border-radius: 100%;
